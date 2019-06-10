486e7785009b7b06b0fdffaf7a86a21a4d3cb8f2
leost3 <leost3@gmail.com>
2019-06-10 18:47:38 -0400

    routers created but link not hooked up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 // let's go!
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-ReactDOM.render(<App />, document.querySelector('#main'));
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route exact path='/' component={StorePicker} />
+                    <Route path='/store/:storeId' component={App} />
+                    <Route component={NotFound} />
+                </Switch>
+            </div>
+            </BrowserRouter>
+    )
+}
+
+ReactDOM.render(<Root />, document.querySelector('#main'));
