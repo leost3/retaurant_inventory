17d5662aa24177fe1171378c9473bf298dc9b076
leost3 <leost3@gmail.com>
2019-06-11 13:02:43 -0400

    page redirected on submit using context

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import {getFunName} from '../helpers';
-
+import PropTypes from 'prop-types';
 class StorePicker extends React.Component {
 
+    submitForm = (e) => {
+        e.preventDefault();
+        const storeiD = this.storeInput.value;
+        this.context.router.history.push("/store/asdasd");
+    }
 
     render() {
         return (
-            <form action="" className="store-selector">
+            <form action="" className="store-selector" onSubmit={this.submitForm}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={ (input) => { this.storeInput = input}}/>
                 <button type="submit">Visit Store</button>
             </form>
         )
     }
+}
+
+
+StorePicker.contextTypes = {
+    router: PropTypes.object
 }
 
 export default StorePicker;
